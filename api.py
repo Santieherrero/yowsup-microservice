14897eb3a02cb33cdb534f1ff9ea5d80b7812f41
Santiago E. Herrero <santieherrero@gmail.com>
2017-10-03 13:18:49 +0200

    Add authorization to send request

--- api.py
+++ api.py
@@ -10,10 +10,14 @@ Swagger(app)
 
 CONFIG = {'AMQP_URI': "pyamqp://guest:guest@localhost"}
 
-@app.route('/send', methods=['POST'])
+@app.route('/send', methods=['GET','POST'])
 @swag_from('docs/send.yml')
 def send():
     logger = app.logger
+    valid_auth = verify_auth(request.headers,logger)
+    logger.info(valid_auth)
+    if not valid_auth:
+        return "Unauthorized for this action", 401
     type = request.json.get('type')
     body = request.json.get('body')
     address = request.json.get('address')
@@ -26,5 +30,16 @@ def send():
     msg = "The message was sucessfully sended to the queue"
     return msg, 200
 
+def verify_auth(headers,logger):
+    if headers["Authorization"]:
+        auth = request.headers["Authorization"]
+        logger.info('Token => ' + auth)
+        if(auth == 'TOKEN_AU'):
+            return True
+        else:
+            return False
+    else:
+        return False
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)
+	app.run(debug=True) #,host='0.0.0.0', port=80
